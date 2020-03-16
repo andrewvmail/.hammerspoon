697ccdec3f9a67a584379ea80698b79aedf8ed69
Andrew Tan <andrewvmail@gmail.com>
2020-03-15 19:12:12 -0700

    Dup + virtual numpad

--- init.lua
+++ init.lua
@@ -93,9 +93,6 @@ hyper:bind({}, "l", function()
   hs.application.launchOrFocus("Trello")
   hyper.triggered = true 
 end)
- hyper:bind({}, "l", function() hs.application.launchOrFocus("Trello")
-  hyper.triggered = true 
-end)
 
 hyper:bind({}, "m", function() 
   hs.application.launchOrFocus("WhatsApp")
@@ -148,4 +145,36 @@ end)
 --   hyper.triggered = true 
 -- end)
 
+k = hs.hotkey.modal.new('ctrl-shift', 'n')
+function k:entered() hs.alert'Virtual Numpad' end
+function k:exited() hs.alert'Exit Virtual Numpad' end
+k:bind('ctrl-shift', 'n', function() k:exit() end)
+
+hs.fnutils.each({
+  { key='j', padkey='pad1'},
+  { key='k', padkey='pad2'},
+  { key='l', padkey='pad3'},
+  { key='u', padkey='pad4'},
+  { key='i', padkey='pad5'},
+  { key='o', padkey='pad6'},
+  { key='7', padkey='pad7'},
+  { key='8', padkey='pad8'},
+  { key='9', padkey='pad9'},
+  { key='m', padkey='pad0'},
+  { key='/', padkey='pad+'},
+  { key=';', padkey='pad-'},
+  { key='p', padkey='pad*'},
+  { key='0', padkey='pad/'},
+}, function(vmap)
+  k:bind({}, vmap.key,
+    function() hs.eventtap.keyStroke({}, vmap.padkey, 20) end,
+    nil,
+    function() hs.eventtap.keyStroke({}, vmap.padkey, 20) end)
+  end
+)
+
+
 hs.alert.show("config loaded")
+
+
+
