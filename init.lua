faf7f13e60aa2e42f705270ab9096e30b35dc736
Andrew Tan <andrewvmail@gmail.com>
2020-04-10 00:37:58 -0700

    ijkl remap to arrow keys

--- init.lua
+++ init.lua
@@ -175,6 +175,31 @@ hs.fnutils.each({
 )
 
 
+
+
+-- https://github.com/kkamdooong/hammerspoon-control-hjkl-to-arrow/blob/master/init.lua
+
+local function pressFn(mods, key)
+  if key == nil then
+    key = mods
+    mods = {}
+  end
+
+  return function() hs.eventtap.keyStroke(mods, key, 1000) end
+end
+
+local function remap(mods, key, pressFn)
+  hs.hotkey.bind(mods, key, pressFn, nil, pressFn)  
+end
+
+remap({'ctrl'}, 'j', pressFn('left'))
+remap({'ctrl'}, 'k', pressFn('down'))
+remap({'ctrl'}, 'i', pressFn('up'))
+remap({'ctrl'}, 'l', pressFn('right'))
+
+
+
+
 hs.alert.show("config loaded")
 
 
