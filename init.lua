6405214932b1cbd79d0293ac64b9fbabf9c112a4
Andrew Tan <andrewvmail@gmail.com>
2020-05-18 15:53:06 -0700

    add xcode + simulator

--- init.lua
+++ init.lua
@@ -71,75 +71,69 @@ end)
 --   hs.alert.show("Gmail")
 -- end)
 
+
 hyper:bind({}, '0', function()
   hs.reload()
   hyper.triggered = true
 end)
 
-hyper:bind({}, "l", function()
-  hs.caffeinate.lockScreen()
-  hyper.triggered = true
-end)
-
-hyper:bind({}, "t", function() 
-  hs.application.launchOrFocus("iTerm")
-  hyper.triggered = true 
-end)
-
-hyper:bind({}, "w", function() 
-  hs.application.launchOrFocus("Webstorm")
-  hyper.triggered = true 
-end)
-
-hyper:bind({}, "i", function() 
-  hs.application.launchOrFocus("IntelliJ Idea")
-  hyper.triggered = true 
-end)
-
-hyper:bind({}, "l", function() 
-  hs.application.launchOrFocus("Trello")
-  hyper.triggered = true 
-end)
-
-hyper:bind({}, "m", function() 
-  hs.application.launchOrFocus("WhatsApp")
-  hyper.triggered = true 
-end)
-
-hyper:bind({}, "c", function() 
-  hs.application.launchOrFocus("Google Chrome")
-  hyper.triggered = true 
-end)
-
+-- adehjknopquvyz
 hyper:bind({}, "b", function() 
   hs.application.launchOrFocus("Brave Browser")
   hyper.triggered = true 
 end)
-
-hyper:bind({}, "l", function() 
-  hs.application.launchOrFocus("Trello")
+hyper:bind({}, "c", function() 
+  hs.application.launchOrFocus("Google Chrome")
   hyper.triggered = true 
 end)
-
-hyper:bind({}, "g", function() 
-  hs.application.launchOrFocus("Gitup")
-  hyper.triggered = true 
-end)
-
-hyper:bind({}, "s", function() 
-  hs.application.launchOrFocus("Sublime Text")
-  hyper.triggered = true 
-end)
-
 hyper:bind({}, "f", function() 
   hs.application.launchOrFocus("Finder")
   hyper.triggered = true 
 end)
+hyper:bind({}, "g", function() 
+  hs.application.launchOrFocus("Gitup")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "i", function() 
+  hs.application.launchOrFocus("IntelliJ Idea")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "l", function() 
+  hs.application.launchOrFocus("Trello")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "m", function() 
+  hs.application.launchOrFocus("WhatsApp")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "r", function() 
+  hs.application.launchOrFocus("Simulator")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "s", function() 
+  hs.application.launchOrFocus("Sublime Text")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "t", function() 
+  hs.application.launchOrFocus("iTerm")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "w", function() 
+  hs.application.launchOrFocus("Webstorm")
+  hyper.triggered = true 
+end)
+hyper:bind({}, "x", function() 
+  hs.application.launchOrFocus("Xcode")
+  hyper.triggered = true 
+end)
+
 
 hyper:bind({}, "`", function() 
   hs.eventtap.keyStroke({"ctrl", "shift"}, "f2")
   hyper.triggered = true 
 end)
+
+
 
 -- open console
 hyper:bind({}, "`", function() 
