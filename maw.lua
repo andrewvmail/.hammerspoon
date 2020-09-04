1ea5cec96e16081af7dbdf0cfc709361a851179e
Andrew Tan <andrewvmail@gmail.com>
2020-09-03 23:07:25 -0700

    Can render text to grid

--- maw.lua
+++ maw.lua
@@ -9,6 +9,12 @@ local spacing = 5 -- viewport h / w
 local increment = 5
 
 local characterToRender = {}
+local grid = {}
+local hintOpt =
+{
+    font = {size = 12},
+    backgroundColor = hs.drawing.color.hammerspoon.osx_yellow,
+}
 
 function dump(o)
     if type(o) == 'table' then
@@ -39,25 +45,26 @@ function numToString(num)
         num = num - remainder;
         num = num / math.floor(base);
     end
-    print(dump(hintString))
+    -- print(dump(hintString))
     return table.concat(hintString, "");
 end
 
-function render_text(canvas, text, x, y)
-    print(x, y)
-    -- canvas:appendElements(
-    --     {
-    --         frame={ h=1,w=2,x=x,y=y },
-    --         text=hs.styledtext.new(
-    --             text,{
-    --                 font={ size=13 },
-    --                 backgroundColor=hs.drawing.color.hammerspoon.osx_yellow,
-    --                 paragraphStyle={
-    --                 maximumLineHeight=13,alignment="top" },
-    --             color={ black=1.0,alpha=1 }}),
-    --             type="text",
-    --         }
-    --     ):show( )
+function renderHints(canvas, grid)
+    -- print(dump(grid))
+    count = 0
+    for k, v in pairs(grid) do
+        count = count + 1
+        if(v["x"] ~= nil and v["y"] ~= nil) then
+            x = v["x"]
+            y = v["y"]
+            opts = {
+                frame = {h = 200, w = 200, x = x, y = y},
+                text = hs.styledtext.new("Momo", hintOpt),
+                type = "text"
+            }
+            canvas:appendElements(opts):show()
+        end
+    end
 end
 
 function listDiagCoordsToTable(listDiagCoords)
@@ -103,21 +110,23 @@ return function(tmod, tkey)
             a = nil
             offsetX = 0
             offsetY = 0
-            exitTap:stop()
+            if(exitTap) then
+                exitTap:stop()
+            end
         elseif code == keycodes['h'] then
-            print("h tapped")
+            -- print("h tapped")
             offsetX = offsetX - increment
             a:transformation(hs.canvas.matrix.translate(offsetX, offsetY))
         elseif code == keycodes['l'] then
-            print("l tapped")
+            -- print("l tapped")
             offsetX = offsetX + increment
             a:transformation(hs.canvas.matrix.translate(offsetX, offsetY))
         elseif code == keycodes['k'] then
-            print("k tapped")
+            -- print("k tapped")
             offsetY = offsetY - increment
             a:transformation(hs.canvas.matrix.translate(offsetX, offsetY))
         elseif code == keycodes['j'] then
-            print("j tapped")
+            -- print("j tapped")
             offsetY = offsetY + increment
             a:transformation(hs.canvas.matrix.translate(offsetX, offsetY))
         end
@@ -144,9 +153,14 @@ return function(tmod, tkey)
         a = c.new{x = 0, y = 0, h = height, w = width}
         
         -- draw vertical lines
+        
         for x = 0, width, H do
+            -- grid[xcounter] = {}
+            -- grid[xcounter]["x"] = x
+            -- xcounter = xcounter + 1
+            
             a:appendElements({
-                action = "line",
+                action = "stroke",
                 closed = false,
                 coordinates = {{x = x, y = 0}, {x = x, y = height}},
                 strokeColor = {red = 1.0},
@@ -156,32 +170,44 @@ return function(tmod, tkey)
         end
         
         -- draw horizontal lines
+        ycounter = 0
         for y = 0, height, W do
+            -- grid[ycounter]["y"] = y
+            
+            local xcounter = 0
+            for x = 0, width, H do
+                gindex = tostring(xcounter)..tostring(ycounter) .. tostring(y) .. tostring(x)
+                grid[gindex] = {}
+                grid[gindex]["x"] = x
+                grid[gindex]["y"] = y
+                
+                -- print(xcounter)
+                xcounter = xcounter + 1
+                
+                -- a:appendElements({
+                --     action = "line",
+                --     closed = false,
+                --     coordinates = {{x = x, y = 0}, {x = x, y = height}},
+                --     strokeColor = {red = 1.0},
+                --     strokeWidth = 3,
+                --     type = "segments",
+                -- })
+            end
+            
             a:appendElements({
-                action = "line",
+                action = "stroke",
                 closed = false,
                 coordinates = {{x = 0, y = y}, {x = width, y = y}},
                 strokeColor = {red = 1.0},
                 strokeWidth = 3,
                 type = "segments",
             })
+            ycounter = ycounter + 1
         end
         
         a:show()
-        
-        print(get_center(54, 62, 5, 16))
-        local x, y = get_center(54, 62, 5, 16)
-        
-        print("111111111111111111111111111111111")
-        print("111111111111111111111111111111111")
-        print(x, y)
-        -- render_text(a,"JJ", x, y)
-        print("111111111111111111111111111111111")
-        print("111111111111111111111111111111111")
-        print(listDiagCoords, 'listDiagCoords')
+        renderHints(a, grid)
         tap:start()
-        
-        -- hs.alert('maw on')
         
     end)
 end
