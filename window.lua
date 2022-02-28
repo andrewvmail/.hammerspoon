13b6d327eb0236ad9e46a494703b1377938c925e
Andrew Tan <andrewvmail@gmail.com>
2022-02-28 02:50:47 -0800

    improve grid

--- window.lua
+++ window.lua
@@ -1,4 +1,11 @@
 hs.grid.ui.textSize = 50
-hs.grid.setGrid('14x14')
+hs.grid.setGrid('10x5')
 hs.grid.setMargins('0x0')
+hs.grid.HINTS = 
+{ 
+	{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }, 
+	{ "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P" }, 
+	{ "A", "S", "D", "F", "G", "H", "J", "K", "L", ";" }, 
+	{ "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/" } 
+}
 hs.hotkey.bind({'cmd'}, 'h', hs.grid.show)
